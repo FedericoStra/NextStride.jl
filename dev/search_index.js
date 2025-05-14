ff47ad58caffa6222e56a345865df71a327f423e
Documenter.jl <documenter@juliadocs.github.io>
2025-05-14 12:37:08 +0000

    build based on 7881d43

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#NextStride","page":"Home","title":"NextStride","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NextStride.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
